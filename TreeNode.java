6bdb442a1da87718fe8ffbdb031b26e37bd915b9
fumika95 <fumikabai@utxas.edu>
2016-02-17 11:40:16 -0600

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
-
+/*
+* original tree node class that is comparable according to it's count
+*
+*/
 public class TreeNode implements Comparable<TreeNode> {
 
 	/*
@@ -9,7 +12,7 @@ public class TreeNode implements Comparable<TreeNode> {
 	private int letter;
 	
 	/*
-	 * own array list of tree node that store children for each node, which contains a-z, A-Z, and 0-9
+	 * own array list of tree node that store children for each node, which contains a-z abd 0-9
 	 */
  	private NodeArray children;
  	
@@ -36,18 +39,28 @@ public class TreeNode implements Comparable<TreeNode> {
  		 this.parent = parent;
  	 }
  	 
+ 	 
  	 public char getLetter(){
  		 return (char)letter;
  	 }
  	 
+ 	 /*
+ 	 * get a child node using the matching char value
+ 	 */
  	 public TreeNode getchild(char letter){
  		 return children.getNode(letter);
  	 }
  	 
+ 	 /*
+ 	 * add a new node to child array in appropriate way
+ 	 */
  	 public boolean setChild(TreeNode newNod){
  		 return children.add(newNod);
  	 }
  	 
+ 	 /*
+ 	 * increment count value of the node
+ 	 */
  	 public int count(){
  		 count++;
  		 return count;
@@ -61,10 +74,16 @@ public class TreeNode implements Comparable<TreeNode> {
  		 return parent;
  	 }
  	 
+ 	 /*
+ 	 * get the node array of children
+ 	 */
  	 public NodeArray getChildren(){
  		 return children;
  	 }
 
+	/*
+	* make the tree node comparable by it's count value
+	*/
 	@Override
 	public int compareTo(TreeNode o) {
 		return o.count-this.count;
