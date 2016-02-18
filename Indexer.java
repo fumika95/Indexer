6c708d5d4edd1e5df48e4c05555e4b8e46505500
fumika95 <fumikabai@utxas.edu>
2016-02-17 20:02:32 -0600

    Update Indexer.java

--- Indexer.java
+++ Indexer.java
@@ -6,29 +6,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 
-/*
-* main of the program that takes file name fromm commanline and run the program to get output
-*/
+
 public class Indexer {
 
 	/*
 	 * tree to store words and count each time
 	 */
-	private static IndexerTree tree;
+	private IndexerTree tree;
 	
 	/*
-	 * array of node that store the final result;
+	 * map that store the final result;
 	 */
-	private static NodeArray result;
+	private NodeArray result;
 	
-	public static void main(String args[]) throws IOException{
+	public Indexer(String args) throws IOException{
 		
 		tree = new IndexerTree();
 		
-		FileInputStream file = new FileInputStream(args[0]);
+		FileInputStream file = new FileInputStream(args);
 		BufferedInputStream buff = new BufferedInputStream(file);
-		int next = buff.read();
 		
+		//start reading file
+		int next = buff.read();
 		while(next!=-1){
 			//start from root node
 			TreeNode cur_node = tree.getRoot();
@@ -36,71 +35,56 @@ public class Indexer {
 			while(next != -1 && !isValid(next)){
 				next = buff.read();
 			}
+			//not end of file
 			if(next != -1){
-			boolean loop = true;
-			while(loop){
-				char letter = (char)next;
-				next = buff.read();
-				//continue adding to child if next is still same word
-				if(next == -1 || !isValid(next))
-					loop = false;
-				//add new node to children
-				int count;
-				if(loop) //is not last char of a word
-					count = 0;
-				else //last
-					count = 1;
-				//
-				//
-				//System.out.println(letter + " " + (char)next + " " + loop);
-				TreeNode nextChild = cur_node.getchild(Character.toLowerCase(letter));
-				if(nextChild == null){
-					TreeNode child = new TreeNode(Character.toLowerCase(letter), cur_node, count);
-					cur_node.setChild(child);
-					cur_node = child;
-				}else if(count ==1){
-					nextChild.count();
-					cur_node = nextChild;
-				}else
-					cur_node = nextChild;
-			}
+				boolean loop = true;
+				while(loop){
+					char letter = (char)next;
+					next = buff.read();
+					//continue adding to child if next is still same word
+					if(next == -1 || !isValid(next))
+						loop = false;
+					//add new node to children
+					int count;
+					if(loop) //is not last char of a word
+						count = 0;
+					else //last
+						count = 1;
+					TreeNode nextChild = cur_node.getchild(Character.toLowerCase(letter));
+					if(nextChild == null){//the letter does not exit yet; add new child
+						TreeNode child = new TreeNode(Character.toLowerCase(letter), cur_node, count);
+						cur_node.setChild(child);
+						cur_node = child;
+					}else if(count ==1){//child exist and end of the word; increment count of word
+						nextChild.count();
+						cur_node = nextChild;
+					}else // child exist but not end of the word yet, keep reading
+						cur_node = nextChild;
+				}
 			}
 		}
-		buff.close()
+		buff.close();
 		
+		//make a list of treenode and sort from 1st to last
 		result = tree.makeList();
 		result.sort();
 		
 		
-		//test1  correct output
-		String[] answer= {"i", "eat", "eggs", "and", "potatos", "bananas", "apples", "peaches", "watermelons", "cookies"};
-		int i = 0;
-		int correct = 0;
-		while(i < 10 && i < result.size()){
-			String user = recursive_print(result.get(i));
-			if(answer[i].equals(user)){
-				correct++;
-			}else{
-				System.out.println("failed correct output  correct:" + answer[i] + " yours: " + user);
-			}
-			i++;
-		}
-		
-		if(correct == i){
-			System.out.println("passed correct output");
-			for(int j = 0; j < 10 ; j++)
-				System.out.println(j+1 + ":  " + answer[j]);
-		}
-		
+	}
+
+	/*
+	* method to return the final result. constructer must be called first
+	*/
+	public NodeArray getResult(){
+		return result;
 	}
 
 	
 
-	
-
-
-
-	private static boolean isValid(int next) {
+	/*
+	*helper method to check is the char value is a valid character or not
+	*/
+	private boolean isValid(int next) {
 		if(next >= 48 && next<= 57) //is number
 			return true;
 		if(next>=65 && next <= 90) // A-Z
@@ -113,7 +97,10 @@ public class Indexer {
 
 
 
-	private static String recursive_print(TreeNode cur_node) {
+	/*
+	* helper method to recursively track each node and get result string to print out
+	*/
+	public String recursive_print(TreeNode cur_node) {
 		
 		String result = "";
 		if(cur_node.getParent() != null){
